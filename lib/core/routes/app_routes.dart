97501f9f08cf84cd77e474119edfaaa5fffd79f0
kareem emad <88625235+kareeeeeeem@users.noreply.github.com>
2025-07-02 14:33:49 +0300

    add files

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tripto/features/splash/presentation/vedio_player_page.dart';
+import 'package:tripto/features/splash/presentation/welcome_home_page.dart';
 import 'package:tripto/features/splash/splash_page.dart';
 
 
 class AppRoutes {
   static const splash = '/';
+  static const home = '/home';
+  static const welcome = '/welcome';
+  static const videoPlayer = '/videoPlayer';
+
+
+
+
 
   static final routes = <String, WidgetBuilder>{
     splash: (context) => const SplashPage(),
+    home: (context) => const WelcomeHomePage(),
+    welcome: (context) => const WelcomeHomePage(),
+    videoPlayer: (context) => const VideoPlayerPage(),    
+
+
   };
 }
