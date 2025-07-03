e436e6ebae2eec4e7c0bb84986dcd1c26e76fb31
mrwan333 <mrwanabdelnasserali12@gmail.com>
2025-07-03 14:24:19 +0300

    git push origin main

---
+++ lib/features/splash/presentation/activities.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class activity {
+
+}
+class activities extends StatelessWidget {
+  const activities({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ,
+    );
+  }
+}
