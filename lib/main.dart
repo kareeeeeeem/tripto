e1ea51930ad0a0da2b0821101961b0068f76403f
mrwan333 <mrwanabdelnasserali12@gmail.com>
2025-07-17 12:20:27 +0300

    git push origin main app localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 // ignore_for_file: prefer_const_constructors
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:tripto/core/routes/app_routes.dart';
+import 'package:tripto/presentation/pagess/Login_pages/Login_page.dart';
+import 'package:tripto/presentation/pagess/Login_pages/SignupOrLogin.dart';
+import 'package:tripto/presentation/pagess/NavBar/ActivityPage/activities_page.dart';
+import 'package:tripto/presentation/pagess/SlideBar/ActivitiesCard.dart';
+
+import 'l10n/app_localizations.dart';
 
 void main() {
   runApp(const TripToApp());
@@ -13,12 +20,23 @@ class TripToApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale('ar'), // أو Locale('ar') للتجربة
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       title: 'TripTo',
       debugShowCheckedModeBanner: false,
 
       initialRoute: '/',
+      // home: const Signuporlogin(),
       routes: AppRoutes.routes,
-      // home: const ActivityCard(),
     );
   }
 }
--- lib/presentation/pagess/NavBar/profile_page.dart
+++ lib/presentation/pagess/NavBar/profile_page.dart
@@ -136,7 +136,7 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ],
           ),
-        ],
+      ),
       ),
     );
   }
